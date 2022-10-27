2291a3f311b47bbe385d9677e12977078e5d7547
sikendrakumarverma <grdskverma2018@gmail.com>
2022-10-27 07:40:05 +0530

    tested

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -139,7 +139,7 @@ const updateBlog = async function (req, res) {
       }
     }
     else {
-      res.status(401).send({ status: false, msg: "Please enter valid Blog id" })
+      res.status(400).send({ status: false, msg: "Please enter valid Blog id" })
     }
   } catch (error) {
     console.log(error.message)
